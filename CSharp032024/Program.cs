26ebb169ce8aee0f3057ba28433674d3844bf989
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2024-04-03 18:00:30 +0200

    Ajout d'un tableau à dexu dimensions pour afficher les départements

--- CSharp032024/Program.cs
+++ CSharp032024/Program.cs
@@ -1,126 +1,70 @@
 ﻿//Faire une application qui nous donne le département en fonction du numéro qu'on lui donne
 string userWantsContinue;
 
+//Comment gérer mes départements d'une meilleure façon ????
+
+string[,] departements = {
+    {"01", "Ain"}, {"02", "Aisne"}, {"03", "Allier"},
+            {"04", "Alpes-de-Haute-Provence"}, {"05", "Hautes-Alpes"},
+            {"06", "Alpes-Maritimes"}, {"07", "Ardèche"}, {"08", "Ardennes"},
+            {"09", "Ariège"}, {"10", "Aube"}, {"11", "Aude"},
+            {"12", "Aveyron"}, {"13", "Bouches-du-Rhône"}, {"14", "Calvados"},
+            {"15", "Cantal"}, {"16", "Charente"}, {"17", "Charente-Maritime"},
+            {"18", "Cher"}, {"19", "Corrèze"}, {"2A", "Corse-du-Sud"},
+            {"2B", "Haute-Corse"}, {"21", "Côte-d'Or"}, {"22", "Côtes-d'Armor"},
+            {"23", "Creuse"}, {"24", "Dordogne"}, {"25", "Doubs"},
+            {"26", "Drôme"}, {"27", "Eure"}, {"28", "Eure-et-Loir"},
+            {"29", "Finistère"}, {"30", "Gard"}, {"31", "Haute-Garonne"},
+            {"32", "Gers"}, {"33", "Gironde"}, {"34", "Hérault"},
+            {"35", "Ille-et-Vilaine"}, {"36", "Indre"}, {"37", "Indre-et-Loire"},
+            {"38", "Isère"}, {"39", "Jura"}, {"40", "Landes"},
+            {"41", "Loir-et-Cher"}, {"42", "Loire"}, {"43", "Haute-Loire"},
+            {"44", "Loire-Atlantique"}, {"45", "Loiret"}, {"46", "Lot"},
+            {"47", "Lot-et-Garonne"}, {"48", "Lozère"}, {"49", "Maine-et-Loire"},
+            {"50", "Manche"}, {"51", "Marne"}, {"52", "Haute-Marne"},
+            {"53", "Mayenne"}, {"54", "Meurthe-et-Moselle"}, {"55", "Meuse"},
+            {"56", "Morbihan"}, {"57", "Moselle"}, {"58", "Nièvre"},
+            {"59", "Nord"}, {"60", "Oise"}, {"61", "Orne"},
+            {"62", "Pas-de-Calais"}, {"63", "Puy-de-Dôme"}, {"64", "Pyrénées-Atlantiques"},
+            {"65", "Hautes-Pyrénées"}, {"66", "Pyrénées-Orientales"}, {"67", "Bas-Rhin"},
+            {"68", "Haut-Rhin"}, {"69", "Rhône"}, {"70", "Haute-Saône"},
+            {"71", "Saône-et-Loire"}, {"72", "Sarthe"}, {"73", "Savoie"},
+            {"74", "Haute-Savoie"}, {"75", "Paris"}, {"76", "Seine-Maritime"},
+            {"77", "Seine-et-Marne"}, {"78", "Yvelines"}, {"79", "Deux-Sèvres"},
+            {"80", "Somme"}, {"81", "Tarn"}, {"82", "Tarn-et-Garonne"},
+            {"83", "Var"}, {"84", "Vaucluse"}, {"85", "Vendée"},
+            {"86", "Vienne"}, {"87", "Haute-Vienne"}, {"88", "Vosges"},
+            {"89", "Yonne"}, {"90", "Territoire de Belfort"}, {"91", "Essonne"},
+            {"92", "Hauts-de-Seine"}, {"93", "Seine-Saint-Denis"}, {"94", "Val-de-Marne"},
+            {"95", "Val-d'Oise"}, {"971", "Guadeloupe"}, {"972", "Martinique"},
+            {"973", "Guyane"}, {"974", "La Réunion"}, {"976", "Mayotte"}
+};
+
+
+
 
 do
 {
     Console.WriteLine("Donne moi le numéro de département à tester");
     string reponse = Console.ReadLine();
-    switch (reponse)
+
+
+
+    for (int i = 0; i < departements.GetLength(0); i++)
     {
-        case "1":
-            Console.WriteLine("Le département est :AIN");
-            break;
-        case "2":
-            Console.WriteLine("Le département est :AISNE");
-            break;
-        case "3":
-            Console.WriteLine("Le département est :ALLIER");
-            break;
-        case "4":
-            Console.WriteLine("Le département est :ALPES-DE-HAUTE-PROVENCE");
-            break;
-        case "5":
-            Console.WriteLine("Le département est :HAUTES-ALPES");
-            break;
-        case "6":
-            Console.WriteLine("Le département est :ALPES-MARITIMES");
-            break;
-        case "7":
-            Console.WriteLine("Le département est :ARDÈCHE");
-            break;
-        case "8":
-            Console.WriteLine("Le département est :ARDENNES");
-            break;
-        case "9":
-            Console.WriteLine("Le département est :ARIÈGE");
-            break;
-        case "10":
-            Console.WriteLine("Le département est :AUBE");
-            break;
-        case "11":
-            Console.WriteLine("Le département est :AUDE");
-            break;
-        case "12":
-            Console.WriteLine("Le département est :AVEYRON");
-            break;
-        case "13":
-            Console.WriteLine("Le département est :BOUCHES-DU-RHÔNE");
-            break;
-        case "14":
-            Console.WriteLine("Le département est :CALVADOS");
-            break;
-        case "15":
-            Console.WriteLine("Le département est :CANTAL");
-            break;
-        case "16":
-            Console.WriteLine("Le département est :CHARENTE");
-            break;
-        case "17":
-            Console.WriteLine("Le département est :CHARENTE-MARITIME");
-            break;
-        case "18":
-            Console.WriteLine("Le département est :CHER");
-            break;
-        case "19":
-            Console.WriteLine("Le département est :CORRÈZE");
-            break;
-        case "21":
-            Console.WriteLine("Le département est :CÔTE-D'OR");
-            break;
-        case "22":
-            Console.WriteLine("Le département est :CÔTES-D'ARMOR");
-            break;
-        case "23":
-            Console.WriteLine("Le département est :CREUSE");
-            break;
-        case "24":
-            Console.WriteLine("Le département est :DORDOGNE");
-            break;
-        case "25":
-            Console.WriteLine("Le département est :DOUBS");
-            break;
-        case "26":
-            Console.WriteLine("Le département est :DRÔME");
-            break;
-        case "27":
-            Console.WriteLine("Le département est :EURE");
-            break;
-        case "28":
-            Console.WriteLine("Le département est :EURE-ET-LOIR");
-            break;
-        case "29":
-            Console.WriteLine("Le département est :FINISTÈRE");
-            break;
-        case "2A":
-            Console.WriteLine("Le département est :CORSE-DU-SUD");
-            break;
-        case "2B":
-            Console.WriteLine("Le département est :HAUTE-CORSE");
-            break;
-        case "971":
-            Console.WriteLine("Le département est :GUADELOUPE");
-            break;
-        case "972":
-            Console.WriteLine("Le département est :MARTINIQUE");
-            break;
-        case "973":
-            Console.WriteLine("Le département est :GUYANE");
-            break;
-        case "974":
-            Console.WriteLine("Le département est :LA RÉUNION");
-            break;
-        case "976":
-            Console.WriteLine("Le département est :MAYOTTE");
-            break;
-        default:
-            Console.WriteLine("Je ne connais pas ce département");
-            break;
+        // { "1", "Ain" }
+        string numDepartement = departements[i, 0];
+        string nomDepartement = departements[i, 1];
+
+        if (reponse.ToLower() == numDepartement.ToLower())
+        {
+            Console.WriteLine(nomDepartement);
+        }
     }
 
 
     Console.WriteLine("");
     Console.WriteLine("Voulez-vous continuer (o/n) ?");
     userWantsContinue = Console.ReadLine();
-} while (userWantsContinue == "o");
+} while (userWantsContinue.ToLower() == "o" );
 
